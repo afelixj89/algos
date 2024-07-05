362b58ffa85e2604bd614e9903c46b22d6bc34ca
Antonio Felix <afelixj89@gmail.com>
2024-07-05 12:35:13 -0400

    removed comments

--- index.js
+++ index.js
@@ -1,69 +1,54 @@
 function twoNumberSum(array, targetSum) {
-    // Create a hash table to store the numbers we've seen so far
-    const numToIndex = 
-      {}
-  
-    // Iterate through the array
-    for (let i = 0; i < array.length; i++) {
-      // Calculate the number needed to reach the target sum
-      const numberNeeded = targetSum - array[i];
-  
-      // Check if the number needed is in the hash table
-      if (numberNeeded in numToIndex) {
-        // If found, return the pair of numbers
-        return [numberNeeded, array[i]];
-      }
-  
-      // Otherwise, add the current number to the hash table
-      numToIndex[array[i]] = i;
+  const numToIndex = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const numberNeeded = targetSum - array[i];
+
+    if (numberNeeded in numToIndex) {
+      return [numberNeeded, array[i]];
     }
-  
-    // If no pair is found, return an empty array
-    return [];
+
+    numToIndex[array[i]] = i;
   }
-  
 
+  return [];
+}
 
+function nonConstructibleChange(coins) {
+  let sortedCoins = coins.sort((a,b) => a-b);
+  let currentChangeCreated = 0;
 
-  function nonConstructibleChange(coins) {
-    let sortedCoins = coins.sort((a,b) => a-b);
-    let currentChangeCreated = 0;
-  
-    for(let i = 0; i < coins.length; i++){
-      let coin = sortedCoins[i];
-      if (coin > currentChangeCreated + 1 ){
-        break;
-      }
-      currentChangeCreated += coin
-      
-      
+  for(let i = 0; i < coins.length; i++){
+    let coin = sortedCoins[i];
+    if (coin > currentChangeCreated + 1 ){
+      break;
     }
+    currentChangeCreated += coin
     
-  return currentChangeCreated + 1
-
-  }
-
-
-  function tournamentWinner(competitions, results){
-    const scores ={};
-    let currentBestTeam = "";
-    scores[currentBestTeam] = 0;
-
-    for(let i = 0; i < competitions.length;i++){
-      const result = results[i];
-      const [homeTeam, awayTeam] = competitions[i];
-      const winningTeam = result === 1 ? homeTeam : awayTeam
-
-      if(!(winningTeam in scores)){
-        scores[winningTeam] = 0
-      }
-      scores[winningTeam] += 3;
-
-      if(scores[winningTeam] > scores[currentBestTeam]){
-        currentBestTeam = winningTeam
-      }
-    }
-    return currentBestTeam
-
+    
   }
   
+return currentChangeCreated + 1
+}
+
+function tournamentWinner(competitions, results) {
+  const scores = {};
+  let currentBestTeam = "";
+  scores[currentBestTeam] = 0;
+
+  for (let i = 0; i < competitions.length; i++) {
+    const result = results[i];
+    const [homeTeam, awayTeam] = competitions[i];
+    const winningTeam = result === 1 ? homeTeam : awayTeam;
+
+    if (!(winningTeam in scores)) {
+      scores[winningTeam] = 0;
+    }
+    scores[winningTeam] += 3;
+
+    if (scores[winningTeam] > scores[currentBestTeam]) {
+      currentBestTeam = winningTeam;
+    }
+  }
+  return currentBestTeam;
+}
