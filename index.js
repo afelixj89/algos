ee38b5a9e3d3c1ba4bc6ce6f55d93285ce737e86
Antonio Felix <afelixj89@gmail.com>
2024-07-15 08:39:49 -0400

    added form algo

--- index.js
+++ index.js
@@ -68,20 +68,18 @@ function transposeMatrix(matrix){
   return newMatrix
 }
 
-function isPalindrome(string) {
-  let leftIdx = 0;
-  let rightIdx = string.length - 1;  
-  while (leftIdx < rightIdx) {
-   
-    if (string[leftIdx] !== string[rightIdx]) {
-      return false;
-    }
-  
-    leftIdx++;
-    rightIdx--;
-  }
-  
-  return true;
-}
+function isPalindrome(string){
+  let leftIndex = 0
+  let rightIndex = string.length - 1;
+  while(leftIndex < rightIndex){
+    if(string[leftIndex] !== string[rightIndex]){
+      return false
 
+    }
+    leftIndex++
+    rightIndex--
+
+  }
+  return true
+}a
 
