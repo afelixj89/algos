2f97c306377e6a9ea32a1a5e43d166b720e99ece
Antonio Felix <afelixjdev@gmail.com>
2024-07-17 09:57:27 -0400

    subsequence

--- index.js
+++ index.js
@@ -171,3 +171,21 @@ list.append(3);
 console.log("Printing the linked list:");
 list.print();
 
+function validateSubsequence(array, sequence){
+  let arrayPointer = 0; 
+  let sequencePointer = 0;
+
+  while(arrayPointer < array.length && sequencePointer < sequence.length){
+    if(array[arrayPointer] === sequence[sequencePointer]){
+      sequencePointer++
+    }
+    arrayPointer++
+  }
+  return sequencePointer === sequence.length 
+  
+}
+
+let array =[5,1,22,25,6,-1,8,10]
+let sequence = [1,6,-1,10]
+
+console.log(validateSubsequence(array, sequence))
