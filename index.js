35549d376715d9ee1a64be1083c158c9788ce2b2
Antonio Felix <afelixjdev@gmail.com>
2024-08-26 16:02:17 -0400

    algo

--- index.js
+++ index.js
@@ -735,4 +735,23 @@ function transposeMatrix(matrix) {
   // }
   
 
+  class LinkedList{
+    constructor(value){
+      this.value = value;
+      this.next = null;
+    }
+  }
   
+  function removeDuplicatesFromLinkedList(linkedList){
+    let currentNode = linkedList;
+  
+    while(currentNode.next !== null){
+      if(currentNode.value === currentNode.next.value){
+        currentNode.next = currentNode.next.next;
+      }
+      else{
+        currentNode = currentNode.next
+      }
+    }
+    return linkedList;
+  }
